77c95d5d0cd0d9ff213e8c4665996531ebb9b888
Joshua Shuster <joshuashuster@Joshuas-MacBook-Pro.local>
2015-05-22 05:39:44 -0700

    progress to basic functionality of the assignment. Unsure of how to handle the passing of the arguments into the second function, as it seems to require an elipsis, unsure on how to proceed, however application seems to be working as desired.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
+# very much like the example, we will be setting the matrix, getting the matrix
+# setting the inverse of the matrix, and gettin the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        
+        setMatrix <- function(matrix){
+                x <<- matrix
+                inverse <<- NULL
+        }
+        getMatrix <- function() x
+        
+        setInverseMatrix <- function(solved_matrix) inverse <<- solved_matrix
+        
+        getInverseMatrix <- function() inverse
+        
+        list(setMatrix = setMatrix, getMatrix = getMatrix, setInverseMatrix = setInverseMatrix, getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+# If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should 
+# retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if(!is.null(x$getInverseMatrix())){
+                # the inverse matrix is defined, so return it 
+                x$getInverseMatrix()
+        }else{
+                # the inverse matrix is not defined, set and return it 
+                inverse <- solve(...)
+                x$setInverseMatrix(inverse)
+        }
+        inverseMatrix <- x$getInverseMatrix()
+        inverseMatrix
 }
